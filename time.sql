cf1d230058cc9a0bbdc92bdf94ff527c5b8e7423
Jeremy Quick <jeremy.quick@bullhorn.com>
2022-02-24 09:52:02 -0600

    Add script for sql statements multiple times and measuring time to execute each individually

---
+++ time.sql
@@ -0,0 +1,28 @@
+-- To measure the performance of a set of sql statements enter the code between the START and END COMMAND comments
+-- To change the number of iterations change all 3 lines with the comment "# of loops"
+
+BEGIN TRANSACTION IncremenetTransaction;
+
+SET XACT_ABORT ON;
+GO 100 -- # of loops
+
+SET NOCOUNT ON;
+
+DECLARE @startTime TIME(7) = GETDATE();
+
+-- START COMMAND
+SELECT TOP 0 0;
+-- END COMMAND
+
+DECLARE @endTime TIME(7) = GETDATE();
+
+DECLARE @elapsed float = DATEDIFF(ms, @startTime, @endTime);
+
+PRINT FORMAT(@elapsed, '00000.00');
+
+SET NOCOUNT OFF;
+
+GO 100 -- # of loops
+
+COMMIT TRANSACTION IncremenetTransaction;
+GO 100 -- # of loops
